02475662dfa9b4d80bc51f052cf6970f4ce64adc
balajisa09 <balajisa09@gmail.com>
2022-11-22 17:47:49 +0530

    Add a simple frontend

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main(){
 	router := gin.Default()
 
 	//register the apis to the router
+	router.StaticFile("/","templates/index.html")
 	router.GET("/healthcheck",handlers.HealthCheck)
 	router.POST("/short",handlers.ShortenURL)
 	router.GET("/admin",handlers.GetURLHashMap)
